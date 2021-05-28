0629ad9c2bc08084dd5b7af5d40dbf862ea6c9d3
Nickarmani03 <nickarmani03@gmail.com>
2021-05-28 11:55:18 -0500

    first commit

---
+++ src/components/customers/Customers.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Customers.css"
+
+export const Customer = () => (
+    <section className="customer">
+        <h3 className="customer__name">Doodles</h3>
+        <div className="customer__address">Address: 100 Infinity way</div>
+    </section>
+)
---
+++ src/components/employees/Employees.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employees.css"
+
+export const Employee = () => (
+    <section className="employee">
+        <h3 className="employee__name">Nate Dogg</h3>
+        <div className="employee__location">Nahsville Kennels North</div>
+    </section>
+)
